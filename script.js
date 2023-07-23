9af1030c26150c56059a744ae15e50de0de3574e
shakilkhan2 <dev.shakilkhan.com>
2023-07-24 01:10:48 +0600

    eventListener doesn't working

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addTask(){
-    if (inputBox.value === ""){
-        alert("Please in add your task.")
-    }else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span)
-    }
-    inputBox.value = "";
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Please in add your task.");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
 }
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    console.log("Event listener callback executed!");
+    if (e.target.tagName === "li") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "span") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
